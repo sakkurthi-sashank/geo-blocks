66457f47d5136800f9bdf1868703ed8e2549ef26
sakkurthi-sashank <sakkurthisashank@gmail.com>
2024-03-30 01:12:57 +0530

    Initialize subgraph

---
+++ eth-mumbai/src/nft-minter.ts
@@ -0,0 +1,353 @@
+import {
+  Approval as ApprovalEvent,
+  ApprovalForAll as ApprovalForAllEvent,
+  BatchMetadataUpdate as BatchMetadataUpdateEvent,
+  DefaultRoyalty as DefaultRoyaltyEvent,
+  EIP712DomainChanged as EIP712DomainChangedEvent,
+  FlatPlatformFeeUpdated as FlatPlatformFeeUpdatedEvent,
+  Initialized as InitializedEvent,
+  MetadataFrozen as MetadataFrozenEvent,
+  MetadataUpdate as MetadataUpdateEvent,
+  OwnerUpdated as OwnerUpdatedEvent,
+  PlatformFeeInfoUpdated as PlatformFeeInfoUpdatedEvent,
+  PlatformFeeTypeUpdated as PlatformFeeTypeUpdatedEvent,
+  PrimarySaleRecipientUpdated as PrimarySaleRecipientUpdatedEvent,
+  RoleAdminChanged as RoleAdminChangedEvent,
+  RoleGranted as RoleGrantedEvent,
+  RoleRevoked as RoleRevokedEvent,
+  RoyaltyForToken as RoyaltyForTokenEvent,
+  TokensMinted as TokensMintedEvent,
+  TokensMintedWithSignature as TokensMintedWithSignatureEvent,
+  Transfer as TransferEvent
+} from "../generated/NFT Minter/NFT Minter"
+import {
+  Approval,
+  ApprovalForAll,
+  BatchMetadataUpdate,
+  DefaultRoyalty,
+  EIP712DomainChanged,
+  FlatPlatformFeeUpdated,
+  Initialized,
+  MetadataFrozen,
+  MetadataUpdate,
+  OwnerUpdated,
+  PlatformFeeInfoUpdated,
+  PlatformFeeTypeUpdated,
+  PrimarySaleRecipientUpdated,
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked,
+  RoyaltyForToken,
+  TokensMinted,
+  TokensMintedWithSignature,
+  Transfer
+} from "../generated/schema"
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.approved = event.params.approved
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let entity = new ApprovalForAll(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.operator = event.params.operator
+  entity.approved = event.params.approved
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleBatchMetadataUpdate(
+  event: BatchMetadataUpdateEvent
+): void {
+  let entity = new BatchMetadataUpdate(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity._fromTokenId = event.params._fromTokenId
+  entity._toTokenId = event.params._toTokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDefaultRoyalty(event: DefaultRoyaltyEvent): void {
+  let entity = new DefaultRoyalty(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.newRoyaltyRecipient = event.params.newRoyaltyRecipient
+  entity.newRoyaltyBps = event.params.newRoyaltyBps
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleEIP712DomainChanged(
+  event: EIP712DomainChangedEvent
+): void {
+  let entity = new EIP712DomainChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleFlatPlatformFeeUpdated(
+  event: FlatPlatformFeeUpdatedEvent
+): void {
+  let entity = new FlatPlatformFeeUpdated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.platformFeeRecipient = event.params.platformFeeRecipient
+  entity.flatFee = event.params.flatFee
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleInitialized(event: InitializedEvent): void {
+  let entity = new Initialized(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.version = event.params.version
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMetadataFrozen(event: MetadataFrozenEvent): void {
+  let entity = new MetadataFrozen(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMetadataUpdate(event: MetadataUpdateEvent): void {
+  let entity = new MetadataUpdate(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity._tokenId = event.params._tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnerUpdated(event: OwnerUpdatedEvent): void {
+  let entity = new OwnerUpdated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.prevOwner = event.params.prevOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handlePlatformFeeInfoUpdated(
+  event: PlatformFeeInfoUpdatedEvent
+): void {
+  let entity = new PlatformFeeInfoUpdated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.platformFeeRecipient = event.params.platformFeeRecipient
+  entity.platformFeeBps = event.params.platformFeeBps
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handlePlatformFeeTypeUpdated(
+  event: PlatformFeeTypeUpdatedEvent
+): void {
+  let entity = new PlatformFeeTypeUpdated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.feeType = event.params.feeType
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handlePrimarySaleRecipientUpdated(
+  event: PrimarySaleRecipientUpdatedEvent
+): void {
+  let entity = new PrimarySaleRecipientUpdated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.recipient = event.params.recipient
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
+  let entity = new RoleAdminChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.previousAdminRole = event.params.previousAdminRole
+  entity.newAdminRole = event.params.newAdminRole
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleGranted(event: RoleGrantedEvent): void {
+  let entity = new RoleGranted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.account = event.params.account
+  entity.sender = event.params.sender
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleRevoked(event: RoleRevokedEvent): void {
+  let entity = new RoleRevoked(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.account = event.params.account
+  entity.sender = event.params.sender
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoyaltyForToken(event: RoyaltyForTokenEvent): void {
+  let entity = new RoyaltyForToken(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.tokenId = event.params.tokenId
+  entity.royaltyRecipient = event.params.royaltyRecipient
+  entity.royaltyBps = event.params.royaltyBps
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTokensMinted(event: TokensMintedEvent): void {
+  let entity = new TokensMinted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.mintedTo = event.params.mintedTo
+  entity.tokenIdMinted = event.params.tokenIdMinted
+  entity.uri = event.params.uri
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTokensMintedWithSignature(
+  event: TokensMintedWithSignatureEvent
+): void {
+  let entity = new TokensMintedWithSignature(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.signer = event.params.signer
+  entity.mintedTo = event.params.mintedTo
+  entity.tokenIdMinted = event.params.tokenIdMinted
+  entity.mintRequest_to = event.params.mintRequest.to
+  entity.mintRequest_royaltyRecipient =
+    event.params.mintRequest.royaltyRecipient
+  entity.mintRequest_royaltyBps = event.params.mintRequest.royaltyBps
+  entity.mintRequest_primarySaleRecipient =
+    event.params.mintRequest.primarySaleRecipient
+  entity.mintRequest_uri = event.params.mintRequest.uri
+  entity.mintRequest_price = event.params.mintRequest.price
+  entity.mintRequest_currency = event.params.mintRequest.currency
+  entity.mintRequest_validityStartTimestamp =
+    event.params.mintRequest.validityStartTimestamp
+  entity.mintRequest_validityEndTimestamp =
+    event.params.mintRequest.validityEndTimestamp
+  entity.mintRequest_uid = event.params.mintRequest.uid
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
